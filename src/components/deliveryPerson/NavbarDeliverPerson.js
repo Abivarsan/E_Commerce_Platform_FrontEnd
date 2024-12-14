72dc71ca8c5847e4aa1744590e8ebda4db9f8b5a
Abivarsan <ketheesabi7400@gmail.com>
2024-12-14 15:18:30 +0530

    logout bug fixed

--- src/components/deliveryPerson/NavbarDeliverPerson.js
+++ src/components/deliveryPerson/NavbarDeliverPerson.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { AppBar, Toolbar, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const NavbarDelivery = () => {
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('userName');
+        localStorage.removeItem('userRole');
+        navigate("/");
+        window.location.reload();
+    };
+    const navigate = useNavigate();
     return (
         <AppBar position="static" sx={{ backgroundColor: '#f06321' }}>
             <Toolbar sx={{ justifyContent: 'flex-end' }}>
@@ -21,6 +31,13 @@ const NavbarDelivery = () => {
                     sx={{ marginLeft: 2, fontWeight: 'bold', color: 'white' }}
                 >
                     My Orders
+                </Button>
+                <Button
+                    color="inherit"
+                    onClick={handleLogout}
+                    sx={{ marginLeft: 2, fontWeight: 'bold', color: 'white' }}
+                >
+                    Logout
                 </Button>
             </Toolbar>
         </AppBar>
